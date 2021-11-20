27ed9afcd4fbf4a1161e52597b8e89f793c55b66
liubc <liubaochen@chinadaas>
2021-11-20 17:14:10 +0800

    添加熔断模块

---
+++ hystrix-products9996/src/main/java/com/chinadaas/HystrixProducts9996Application.java
@@ -0,0 +1,13 @@
+package com.chinadaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixProducts9996Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixProducts9996Application.class, args);
+    }
+
+}
