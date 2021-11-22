a62946dfd881ce638ea7679b9afae5241fbc7a55
liubc <liubaochen@chinadaas>
2021-11-22 10:46:20 +0800

    添加一个带熔断的用户服务

---
+++ hystrix-users10000/src/main/java/com/chinadaas/HystrixUsers10000Application.java
@@ -0,0 +1,13 @@
+package com.chinadaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixUsers10000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixUsers10000Application.class, args);
+    }
+
+}
---
+++ hystrix-users10000/src/test/java/com/chinadaas/HystrixUsers10000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinadaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixUsers10000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
